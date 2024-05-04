1e1b433338a8fd64f02028033b2b50793ca35cc0
BrianWalsh23 <G00424410@atu.ie>
2024-05-04 18:06:47 +0100

    commit

--- weatherapp/src/app/signup/signup.page.ts
+++ weatherapp/src/app/signup/signup.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonItem, IonInput, IonIcon } from '@ionic/angular/standalone';
+import {  IonTitle, } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonInput, IonItem, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonBackButton, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonTitle, CommonModule, FormsModule, IonicModule]
 })
 export class SignupPage implements OnInit {
 
