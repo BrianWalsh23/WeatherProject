eac8e9721ef8f9e2115cc858d675c4c33ec2bd97
BrianWalsh23 <G00424410@atu.ie>
2024-05-05 23:50:19 +0100

    commit

--- weatherapp/src/app/app.component.ts
+++ weatherapp/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { IonicModule } from '@ionic/angular';
+import{ RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports:[IonicModule],
+  imports:[IonicModule, RouterOutlet, CommonModule,],
 })
 export class AppComponent {
   constructor(private afAuth: AngularFireAuth) {
